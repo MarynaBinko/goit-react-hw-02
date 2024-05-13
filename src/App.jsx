ff2da4ddafb0ad025d2099a76be17222be6971a8
Maryna Binko <benyhorses@hotmail.com>
2024-05-13 11:14:25 +0100

    components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 
 import './App.css'
+import Description from './components/Description/Description'
+import Feedback from './components/Feedback/Feedback'
+import Options from './components/Options/Options'
 
 function App() {
   
 
   return (
     <>
-
+     <Description />
+     <Options />
+     <Feedback />
     </>
   )
 }
---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,13 @@
+
+
+const Feedback = () => {
+  return (
+    <div>
+      <button>Good</button>
+      <button>Neutral</button>
+      <button>Bad</button>
+    </div>
+  )
+}
+
+export default Feedback
