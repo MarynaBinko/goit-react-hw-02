1a3f8966ef08ca479c7a48dc24e868447d2989e7
Maryna Binko <benyhorses@hotmail.com>
2024-05-15 13:14:25 +0100

    styles added

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,18 +1,18 @@
-
+import styles from "./Feedback.module.css"
 
 const Feedback = ( {optionsType, totalFeedback, positiveFeedback} ) => {
  
   return (
-    <div>
+    <div className={styles.feedback}>
       
-      <ul>
-        <li>Good: {optionsType.good}</li>
-        <li>Neutral:{optionsType.neutral} </li>
-        <li>Bad:{optionsType.bad} </li>
+      <ul className={styles.list}>
+        <li className={styles.listItem}>Good: {optionsType.good}</li>
+        <li className={styles.listItem}>Neutral: {optionsType.neutral} </li>
+        <li className={styles.listItem}>Bad: {optionsType.bad} </li>
       </ul>
-      <h2>Feedback Statistics</h2>
-      <p>Total Feedback: {totalFeedback}</p>
-      <p>Percentage of Positive Feedback: {positiveFeedback}%</p>
+      <h2 className={styles.title}>Feedback Statistics</h2>
+      <p className={styles.feed}>Total Feedback: {totalFeedback}</p>
+      <p className={styles.feed}>Percentage of Positive Feedback: {positiveFeedback}%</p>
     </div>
   )
 }
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,15 +1,17 @@
+import styles from "./Options.module.css"
+
 
 const Options = ({ updateFeedback, totalFeedback, handleReset }) => {
 
 
 
   return (
-    <div>
-      <button onClick= {() => updateFeedback("good")}>Good</button>
-      <button onClick= {() => updateFeedback("neutral")}>Neutral</button>
-      <button onClick= {() => updateFeedback("bad")}>Bad</button>
+    <div className={styles.options}>
+      <button className={styles.btnClick} onClick= {() => updateFeedback("good")}>Good</button>
+      <button className={styles.btnClick} onClick= {() => updateFeedback("neutral")}>Neutral</button>
+      <button className={styles.btnClick} onClick= {() => updateFeedback("bad")}>Bad</button>
       {totalFeedback > 0 && (
-        <button onClick={handleReset}>Reset</button>
+        <button className={styles.btnClick} onClick={handleReset}>Reset</button>
       )}
     </div>
   )
